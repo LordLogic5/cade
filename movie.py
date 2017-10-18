e98da9d0dd77ce06d5580739e7b766b281e22e98
logic5 <johnlovejoy@Johns-MacBook.local>
2017-10-18 16:59:41 -0500

    added movie.py

---
+++ movie.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""A short python example code being pulled and pushed onto
+github"""
+
+def main():
+  print("Woohoo here we go!")
+  movie = input("Please enter in your favorite movie!: ")
+  if movie == "Red":
+    print("You have selected an excellent film!")
+    else:
+    print("Your selection is not the best.")
+
+if __name__ == "__main__":
+  main()
